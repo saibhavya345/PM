05d26da7e02b3525adc208dd8ef61a20a9a217a0
SaiBhavya <saibhavya2345@gmail.com>
2024-02-14 11:56:13 +0530

    first commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,8 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { HomeComponent } from './home/home.component';
 import { TargetComponent } from './target/target.component';
 
 const routes: Routes = [
-  {path:'home',component:HomeComponent},
   {path:'target',component:TargetComponent}
 
 ];
